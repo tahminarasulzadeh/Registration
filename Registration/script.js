11860671eabb19e345baa26ab73fe4d47f6679a3
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2021-12-21 22:12:33 +0400

    Add files via upload

---
+++ Registration/script.js
@@ -0,0 +1,12 @@
+function test() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (password == "12345" && email == "alizadeh@gmail.com") {
+    alert("sizin sayta daxil ola bildiniz");
+  } else {
+    alert("siz sayta daxil ola bilmədiniz");
+  }
+}
+
+
